0c132d90b93575fc6e4b91eb837e3434d69312af
Toivo Vaje <toivo.vaje@gmail.com>
2014-05-15 22:39:37 +0300

    more random stuff

--- scripts.js
+++ scripts.js
@@ -27,13 +27,13 @@ function init() {
 function draw() {
 
     var time = new Date().getTime() * 0.002;
-    var x = Math.sin( time ) * 192 + 256;
-    var y = Math.cos( time * 0.9 ) * 192 + 256;
+    var x = Math.sin( time ) * 192 * Math.random() + 256;
+    var y = Math.cos( time * 0.9 ) * 192 * Math.random() + 256;
     toggle = !toggle;
 
     context.fillStyle = toggle ? 'rgb(200,200,20)' :  'rgb(20,20,200)';
     context.beginPath();
-    context.arc( x, y, 10, 0, Math.PI * 2, true );
+    context.arc( x, y, 10* Math.random(), 0, Math.PI * 2, true );
     context.closePath();
     context.fill();
 }
